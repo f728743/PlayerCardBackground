620bbc1bf1cc9584957693254780cc5870a48018
Alexey Vorobyov <vorobyov.alexey@gmail.com>
2022-03-24 10:48:01 +0300

    Initial Commit

---
+++ PlayerCardBackground/PlayerCardBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerCardBackgroundApp.swift
+//  PlayerCardBackground
+//
+//  Created by Alexey Vorobyov on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PlayerCardBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
