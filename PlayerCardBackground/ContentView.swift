496d752b6c2f9a27aaa1ea2c57b3e46961de4b5d
Alexey Vorobyov <vorobyov.alexey@gmail.com>
2022-03-24 13:53:05 +0300

    refactoring

--- PlayerCardBackground/ContentView.swift
+++ PlayerCardBackground/ContentView.swift
@@ -5,36 +5,55 @@
 //  Created by Alexey Vorobyov on 24.03.2022.
 //
 
-import SwiftUI
 import ColorKit
+import SwiftUI
+
+extension UIImage {
+    func convertToRGBColorspace() -> UIImage? {
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(
+            data: nil,
+            width: Int(size.width),
+            height: Int(size.height),
+            bitsPerComponent: 8,
+            bytesPerRow: 0,
+            space: CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: UInt32(bitmapInfo.rawValue)
+        )
+        guard let context = context, let cgImage = cgImage else { return nil }
+        context.draw(cgImage, in: CGRect(origin: CGPoint.zero, size: size))
+        guard let convertedImage = context.makeImage() else { return nil }
+        return UIImage(cgImage: convertedImage)
+    }
+}
 
 struct ContentView: View {
-    
     let images = [
         "radio_01_class_rock", "radio_02_pop", "radio_03_hiphop_new",
         "radio_04_punk", "radio_05_talk_01", "radio_06_country",
         "radio_07_dance_01", "radio_08_mexican", "radio_09_hiphop_old",
         "radio_11_talk_02", "radio_12_reggae", "radio_13_jazz",
         "radio_14_dance_02", "radio_15_motown", "radio_16_silverlake",
-        "radio_17_funk", "radio_18_90s_rock"
+        "radio_17_funk", "radio_18_90s_rock",
     ]
-    
+
     @State var imageIndex = 0
-    
+
     @State var colors: [(id: Int, color: UIColor, frequency: CGFloat)] = []
-    
+    @State var gradietnModel = AnimatedGradient.Model(colors: [])
+
     private var columns = Array(
         repeating: GridItem(.flexible(), spacing: Constants.padding),
         count: 5
     )
-    
+
     var body: some View {
         VStack {
             Image(uiImage: image)
                 .resizable()
                 .scaledToFit()
                 .frame(width: Constants.coverArtSize, height: Constants.coverArtSize)
-                
+
                 .background(
                     Color(uiColor: .systemGray4)
                 )
@@ -44,10 +63,9 @@ struct ContentView: View {
                         .stroke(Color(UIColor.systemGray3), lineWidth: 1)
                 )
                 .padding(.top, 40)
-            
-            
+
             changeImageButtons.padding()
-            
+
             ScrollView {
                 LazyVGrid(columns: columns, spacing: Constants.padding) {
                     ForEach(colors, id: \.id) { color in
@@ -68,14 +86,16 @@ struct ContentView: View {
             }
             .padding()
         }
-        .ignoresSafeArea()
+
         .background(
-            GradientEffectView(colors: colors.prefix(5).map { $0.color })
+            //            GradientEffectView(colors: colors.prefix(5).map(\.color))
+//            Color.blue
+            GradientEffectView($gradietnModel)
         )
+        .ignoresSafeArea()
         .onAppear(perform: updateColors)
-        
     }
-    
+
     var changeImageButtons: some View {
         HStack {
             Button(action: backward) { Image(systemName: "backward.fill") }
@@ -84,15 +104,13 @@ struct ContentView: View {
         .foregroundColor(.primary)
         .font(.largeTitle)
     }
-        
 }
 
 private extension ContentView {
-    
     var image: UIImage {
-         UIImage(named: images[imageIndex])?.convertToRGBColorspace() ??  UIImage()
+        UIImage(named: images[imageIndex])?.convertToRGBColorspace() ?? UIImage()
     }
-    
+
     func backward() {
         guard !images.isEmpty else { return }
         imageIndex -= 1
@@ -101,7 +119,7 @@ private extension ContentView {
         }
         updateColors()
     }
-    
+
     func forward() {
         guard !images.isEmpty else { return }
         imageIndex += 1
@@ -110,14 +128,19 @@ private extension ContentView {
         }
         updateColors()
     }
-    
+
     func updateColors() {
-        guard let dominantColors = try? image.dominantColorFrequencies(with: .fair) else { return }
+        guard let dominantColors = try? image.dominantColorFrequencies(with: .high) else { return }
         colors = dominantColors
+            .prefix(5)
             .enumerated()
             .map { ($0.offset, $0.element.color, $0.element.frequency) }
+
+        withAnimation(.linear.speed(0.2)) {
+            gradietnModel.colors = colors.map { Color(uiColor: $0.color) }
+        }
     }
-    
+
     enum Constants {
         static let coverArtSize: CGFloat = 200
         static let cornerRadius: CGFloat = 6
@@ -126,8 +149,6 @@ private extension ContentView {
         static let colorCornerRadius: CGFloat = 3
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- PlayerCardBackground/GradientEffectView.swift
+++ PlayerCardBackground/GradientEffectView.swift
@@ -5,124 +5,53 @@
 //  Created by Alexey Vorobyov on 24.03.2022.
 //
 
+import Combine
 import SwiftUI
 
-extension UIImage {
-    func convertToRGBColorspace() -> UIImage? {
-        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-        let context = CGContext.init(
-            data: nil,
-            width: Int(size.width),
-            height: Int(size.height),
-            bitsPerComponent: 8,
-            bytesPerRow: 0,
-            space: CGColorSpaceCreateDeviceRGB(),
-            bitmapInfo: UInt32(bitmapInfo.rawValue)
-        )
-        guard let context = context, let cgImage = cgImage else { return nil }
-        context.draw(cgImage, in: CGRect(origin: CGPoint.zero, size: size))
-        guard let convertedImage = context.makeImage() else { return nil }
-        return UIImage(cgImage: convertedImage)
-    }
-    
-    
-    static func gradientImageWithBounds(size: CGSize, colors: [UIColor]) -> UIImage {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = CGRect(origin: .zero, size: size)
-        gradientLayer.colors = colors.map { $0.cgColor }
-        
-        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
-        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
-    }
-}
+struct AnimatedGradient: View {
+    struct Model {
+        init(colors: [Color]) {
+            firstGradientColors = colors
+        }
 
-struct GradientEffectView: View {
-    
-    @State var animation = false
-    
-    
-    let colors: [UIColor]
-    
+        var colors: [Color] {
+            get {
+                isFirstGradientVisible ? firstGradientColors : secondGradientColors
+            }
+            set {
+                if isFirstGradientVisible {
+                    secondGradientColors = newValue
+                } else {
+                    firstGradientColors = newValue
+                }
+                isFirstGradientVisible.toggle()
+            }
+        }
+
+        fileprivate var isFirstGradientVisible = true
+        fileprivate var firstGradientColors: [Color]
+        fileprivate var secondGradientColors: [Color] = []
+    }
+
+    @Binding private var model: Model
+
+    init(_ model: Binding<Model>) {
+        _model = model
+    }
+
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                firstImage(geometry)
-                image(geometry)
-                image(geometry)
-                image(geometry)
-//                VisualEffect(effect: UIBlurEffect(style: .systemUltraThinMaterialDark))
-                VisualEffect(effect: UIBlurEffect(style: .systemUltraThinMaterial))
-            }
-            .frame(width: geometry.size.width, height: geometry.size.height)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: model.firstGradientColors),
+                startPoint: .top,
+                endPoint: .bottom
+            ).opacity(model.isFirstGradientVisible ? 1 : 0)
+            LinearGradient(
+                gradient: Gradient(colors: model.secondGradientColors),
+                startPoint: .top,
+                endPoint: .bottom
+            ).opacity(model.isFirstGradientVisible ? 0 : 1)
         }
-        .edgesIgnoringSafeArea(.all)
-        .onAppear {
-            withAnimation(Animation.linear(duration: 100).repeatForever()) {
-                animation.toggle()
-            }
-        }
-    }
-    
-    var gradient: UIImage {
-        return UIImage.gradientImageWithBounds(
-            size: CGSize(width: 400, height: 400),
-            colors: colors.count == 1 ? [colors[0], colors[0]] : colors
-        )
-    }
-    
-    func image(_ geometry: GeometryProxy) -> some View {
-        Image(uiImage: gradient)
-            .resizable()
-            .frame(
-                width: randomFrame(geometry.size.width),
-                height: randomFrame(geometry.size.width)
-            )
-            .scaleEffect(randomCGFloat(in: 1...2.5))
-            .opacity(0.5)
-            .rotationEffect(.degrees(randomDouble(in: -360...360)), anchor: .center)
-            .offset(x: randomCGFloat(in: -300...300), y: randomCGFloat(in: -300...300))
-            .blendMode(.lighten)
-            .saturation(randomDouble(in: 0.4...1.4))
-            .contrast(2)
-    }
-    
-    func firstImage(_ geometry: GeometryProxy) -> some View {
-        Image(uiImage: gradient)
-            .resizable()
-//            .brightness(-0.5)
-            .rotationEffect(.degrees(randomDouble(in: -360...360)), anchor: .center)
-            .frame(width: geometry.size.height*2, height: geometry.size.height*2)
-    }
-    
-    func randomFrame(_ base: CGFloat) -> CGFloat {
-        let randomNumber = animation ? CGFloat.random(in: -100...300) : CGFloat.random(in: -100...300)
-        let frame = base + randomNumber
-        return frame
-    }
-    
-    func randomCGFloat(in range: ClosedRange<CGFloat>) -> CGFloat {
-        let randomNumber = animation ? CGFloat.random(in: range) : CGFloat.random(in: range)
-        return randomNumber
-    }
-    
-    func randomDouble(in range: ClosedRange<Double>) -> Double {
-        let randomNumber = animation ? Double.random(in: range) : Double.random(in: range)
-        return randomNumber
-    }
-}
-
-struct GradientEffectView_Previews: PreviewProvider {
-    static var previews: some View {
-        GradientEffectView(
-            colors: [
-                .green,
-                UIColor(red: 1, green: 0, blue: 0, alpha: 1),
-                UIColor(red: 0, green: 1, blue: 0, alpha: 1)
-            ]
-        )
     }
 }
 
@@ -130,10 +59,111 @@ struct VisualEffect: UIViewRepresentable {
     var effect: UIVisualEffect?
     let effectView = UIVisualEffectView(effect: nil)
 
-    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
+    func makeUIView(context _: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
         effectView.effect = effect
         return effectView
     }
 
-    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { }
+    func updateUIView(_: UIVisualEffectView, context _: UIViewRepresentableContext<Self>) {}
+}
+
+struct GradientEffectView: View {
+    private struct Blot: Identifiable {
+        let id = UUID()
+        let sizeModifier: CGSize
+        let scale: CGFloat
+        let rotation: Angle
+        let offset: CGPoint
+        let saturation: Double
+
+        static var random: Blot {
+            Blot(
+                sizeModifier: CGSize(width: .random(in: -100 ... 300), height: .random(in: -100 ... 300)),
+                scale: .random(in: 1 ... 2.5),
+                rotation: .degrees(.random(in: 0 ... 360)),
+                offset: CGPoint(x: .random(in: -300 ... 300), y: .random(in: -300 ... 300)),
+                saturation: .random(in: 0.4 ... 1.4)
+            )
+        }
+    }
+
+    let timerUpdate = 5.0
+
+    @State private var backgroundAngle: Angle = .zero
+    @State private var blots: [Blot] = (0 ..< 3).map { _ in .random }
+    @Binding private var model: AnimatedGradient.Model
+    private let timer: Publishers.Autoconnect<Timer.TimerPublisher>
+
+    init(_ model: Binding<AnimatedGradient.Model>) {
+        _model = model
+        timer = Timer.publish(every: timerUpdate, on: .main, in: .common).autoconnect()
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                background(geometry)
+                blot(withParams: blots[0], geometry)
+                blot(withParams: blots[1], geometry)
+                blot(withParams: blots[2], geometry)
+                VisualEffect(effect: UIBlurEffect(style: .systemUltraThinMaterial))
+                VisualEffect(effect: UIBlurEffect(style: .systemUltraThinMaterialDark))
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .onAppear {
+                update()
+            }
+            .onReceive(timer) { _ in
+                update()
+            }
+            .onChange(of: model.isFirstGradientVisible) { _ in
+                update()
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+    }
+
+    private func update() {
+        withAnimation(.easeInOut(duration: timerUpdate).speed(0.1)) {
+            backgroundAngle = .degrees(.random(in: 0 ... 360))
+            blots = (0 ..< blots.count).map { _ in .random }
+        }
+    }
+
+    private func background(_ geometry: GeometryProxy) -> some View {
+        let size = hypot(geometry.size.width, geometry.size.height)
+        return AnimatedGradient($model)
+            .rotationEffect(backgroundAngle, anchor: .center)
+            .frame(width: size, height: size)
+    }
+
+    private func blot(withParams blot: Blot, _ geometry: GeometryProxy) -> some View {
+        let size = min(geometry.size.width, geometry.size.height)
+        return AnimatedGradient($model)
+            .clipShape(Capsule())
+            .frame(
+                width: size + blot.sizeModifier.width,
+                height: size + blot.sizeModifier.height
+            )
+            .scaleEffect(blot.scale)
+            .opacity(0.5)
+            .rotationEffect(blot.rotation, anchor: .center)
+            .offset(x: blot.offset.x, y: blot.offset.y)
+            .blendMode(.lighten)
+            .saturation(blot.saturation)
+            .contrast(2)
+    }
+}
+
+struct GradientEffectView_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientEffectView(
+            .constant(
+                AnimatedGradient.Model(
+                    colors: [.red, .yellow, .green, .blue, .magenta]
+                        .map { Color(uiColor: $0) }
+                )
+            )
+        )
+    }
 }
